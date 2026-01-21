31f5bc019b3ba9806b7a6f3382e030004973b07e
Tanisha241 <tanisha.2426mca634@kiet.edu>
2026-01-21 12:01:05 +0530

    Add files via upload

---
+++ LPattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+   
+    for(i = 1; i <= 4; i++) {
+        printf("*\n");
+    }
+
+ L
+    for(i = 1; i <= 5; i++) {
+        printf("* ");
+    }
+
+    return 0;
+}
